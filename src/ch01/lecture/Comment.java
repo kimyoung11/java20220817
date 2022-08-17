26d6c2429d03c190bde4f8f00aeefa31da08e1ff
kimyoung11 <kim_young_@naver.com>
2022-08-17 12:15:51 +0900

    first commit

---
+++ src/ch01/lecture/Comment.java
@@ -0,0 +1,12 @@
+package ch01.lecture;
+
+public class Comment {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("자바는 쉽지않다.");
+		System.out.println("잘 배워서 써먹어 보자"); //실행에 영향을 미치지 않음
+		
+		
+	}
+}
---
+++ src/java20220817/HelloWorld.java
@@ -0,0 +1,8 @@
+package java20220817;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!"); 
+	}
+}
